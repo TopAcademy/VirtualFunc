40163c04cb1c64ef3fd2d2ab82c4f53c45207852
Max Huzmiev <max@huzm.ru>
2022-08-03 10:05:49 +0300

    ForSale class added

---
+++ ForSale.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class ForSale
+{
+protected:
+	int rub;
+	short kop;
+public:
+	ForSale();
+	void set_price(int _rub, short _kop);
+	float get_price();
+};
+
+ForSale::ForSale()
+{
+	rub = 0; kop = 0;
+}
+
+void ForSale::set_price(int _rub, short _kop)
+{
+	rub = _rub;
+	if (_kop < 100 && _kop > 0) kop = _kop;
+	else kop = 0;
+}
+
+float ForSale::get_price()
+{
+	float res = rub + (float)kop / 100;
+	return res;
+}
